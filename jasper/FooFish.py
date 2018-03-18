ebe7e274b8103f72da80ffb5ac6826474a855341
geethanjali hv <geethanjalihv@geethanjalis-MacBook-Air.local>
2018-03-17 18:47:58 -0700

    FooFish.py module added

---
+++ jasper/FooFish.py
@@ -0,0 +1,37 @@
+ -*- coding: utf-8-*-
+import random
+import re
+
+WORDS = ["FISH", "HUNGRY", "FEED", "HUNGRY"]
+
+
+def handle(text, mic, profile):
+    """
+        Responds to user-input, typically speech text, by relaying the
+        meaning of life.
+
+        Arguments:
+        text -- user-input, typically transcribed speech
+        mic -- used to interact with the user (for both input and output)
+        profile -- contains information related to the user (e.g., phone
+                   number)
+    """
+    messages = ["Feeding the fish now, Geethanjali"]
+
+
+    message = random.choice(messages)
+    print message
+
+    mic.say(message)
+
+
+def isValid(text):
+    """
+        Returns True if the input is related to the meaning of life.
+
+        Arguments:
+        text -- user-input, typically transcribed speech
+    """
+    return bool(re.search(r'\bfish\b', text, re.IGNORECASE))
+~                                                                               
+
