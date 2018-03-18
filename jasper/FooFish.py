7a0c1a6d15ef6e018741782bb37136d179757f1a
hvanjali002 <hvanjali002@gmail.com>
2018-03-18 00:03:37 -0700

    Send Serial data to Raspberry Pi for running the motor

--- jasper/FooFish.py
+++ jasper/FooFish.py
@@ -1,14 +1,35 @@
- -*- coding: utf-8-*-
+# -*- coding: utf-8-*-
 import random
 import re
+import serial
+import time
 
-WORDS = ["FISH", "HUNGRY", "FEED", "HUNGRY"]
+ser = serial.Serial(
+               port='/dev/ttyUSB0',
+               baudrate = 9600,
+               parity=serial.PARITY_NONE,
+               stopbits=serial.STOPBITS_ONE,
+               bytesize=serial.EIGHTBITS,
+               timeout=1
+           )
+counter = 0
+
+def init_conn():
+        ser.write('AT')
+        time.sleep(1)
+        ser.write('AT+CONNL')
+        time.sleep(1)
+        ser.write('AT+CONNL')
+        time.sleep(1)
+
+
+
+WORDS = ["FISH", "AQUARIUM",  "FEED", "HUNGRY"]
 
 
 def handle(text, mic, profile):
     """
-        Responds to user-input, typically speech text, by relaying the
-        meaning of life.
+        Responds to user-input, typically speech text.
 
         Arguments:
         text -- user-input, typically transcribed speech
@@ -16,22 +37,24 @@ def handle(text, mic, profile):
         profile -- contains information related to the user (e.g., phone
                    number)
     """
-    messages = ["Feeding the fish now, Geethanjali"]
+    messages = ["Feeding the fish now, Geethanjali",
+                "Wow, Fish is happy now",
+                "Fish says yummy yummy in my tummy"]
 
+    init_conn()
+    ser.write('FEED NOW\n')
 
     message = random.choice(messages)
     print message
 
     mic.say(message)
 
-
 def isValid(text):
     """
-        Returns True if the input is related to the meaning of life.
+        Returns True if the input is related to the FooFish.
 
         Arguments:
         text -- user-input, typically transcribed speech
     """
     return bool(re.search(r'\bfish\b', text, re.IGNORECASE))
-~                                                                               
 
